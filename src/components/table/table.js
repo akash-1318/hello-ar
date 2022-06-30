800d16adce2b6b2bd3e1bfc1f7f71e9529938240
akash18baaz <akashsharma@Akashs-MacBook-Air.local>
2022-06-30 16:03:39 +0530

    refactor - classname change

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,42 +1,48 @@
-import "./table.css"
+import "./table.css";
 
-function Table(){
-    return(
-        <table class="styled-table">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>User</th>
-                  <th>Last Signed In</th>
-                  <th>Role</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Akash</td>
-                  <td>1 hour</td>
-                  <td>owner</td>
-                  <td><i class='bx bx-trash' ></i></td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Akash</td>
-                  <td>1 hour</td>
-                  <td>owner</td>
-                  <td><i class='bx bx-trash' ></i></td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>Akash</td>
-                  <td>1 hour</td>
-                  <td>owner</td>
-                  <td><i class='bx bx-trash' ></i></td>
-                </tr>
-              </tbody>
-            </table>
-    )
+function Table() {
+  return (
+    <table class="styled__table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>User</th>
+          <th>Last Signed In</th>
+          <th>Role</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Akash</td>
+          <td>1 hour</td>
+          <td>owner</td>
+          <td>
+            <i class="bx bx-trash"></i>
+          </td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Akash</td>
+          <td>1 hour</td>
+          <td>owner</td>
+          <td>
+            <i class="bx bx-trash"></i>
+          </td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Akash</td>
+          <td>1 hour</td>
+          <td>owner</td>
+          <td>
+            <i class="bx bx-trash"></i>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
-export {Table}
+export { Table };
